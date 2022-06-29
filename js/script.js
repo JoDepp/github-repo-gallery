2b8ffebdd1b4125b01beacc2f3fc356bbd91e2c0
jody_d <jodydepp@gmail.com>
2022-06-29 16:23:19 -0400

    ADDED repos to webpage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //this is the div where my profile info will appear
 const overview = document.querySelector(".overview");
 const username = "JoDepp";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function() {
     const userInfo = await fetch(
@@ -26,4 +27,22 @@ const displayUserInfo = function (data) {
         </div>
         `;
         overview.append(div);  //append the div to the overview element
+        fetchRepos();  //call the function below
+};
+
+const fetchRepos = async function() {   //select my repos and sort by recently updated up to 100
+    const myRepos = await fetch(  `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const returnRepos = await myRepos.json();  //return json respons
+    repoInfo(returnRepos);
+   // console.log(returnRepos);
+   repoInfo(returnRepos);
+};
+
+const repoInfo = function(repos) {  //using repos as the parameter allows the function to accept the data from last API call
+    for( const repo of repos) {   //loop an create a list item of each repo
+        const repoItem = document.createElement("li");   //creates a new HTML element
+        repoItem.classList.add("repo");   //adds a new class
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);  //appends the list item to the global variable that selects the unordered repos list
+    }
 };
