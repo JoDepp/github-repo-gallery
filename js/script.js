dd3ee2c4ab8bc997233627cd311609e7be1f0160
jody_d <jodydepp@gmail.com>
2024-02-05 13:02:59 -0500

    added some comments

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 
-const overview = document.querySelector(".overview");
-const username = "JoDepp";
-const repoList = document.querySelector(".repo-list");
-const myRepoInfo = document.querySelector(".repos");
-const repoData = document.querySelector(".repo-data");
-const viewReposButton = document.querySelector(".view-repos");
 const filterInput = document.querySelector(".filter-repos");
+const myRepoInfo = document.querySelector(".repos"); //selects repos where all repo info appears
+const overview = document.querySelector(".overview"); //selects div with class overview
+const repoList = document.querySelector(".repo-list"); //select UL to display repo list
+const repoData = document.querySelector(".repo-data"); //repo-data is where individual repo data will appear
+const username = "JoDepp"; //github user name
+const viewReposButton = document.querySelector(".view-repos");
 
+//1st async function fetches info from my github profile using API address and JSON
 const gitUserInfo = async function() {
     const userInfo = await fetch(
-        `https://api.github.com/users/${username}`); //targets the "users" endpoint, the $ creates a placeholder
-    const data = await userInfo.json();
-    displayUserInfo(data);
+        `https://api.github.com/users/${username}`); // githubs api address targets the "users" endpoint, the $ creates a placeholder
+    const data = await userInfo.json();  //tells program to wait until API data is received from JSON
+    displayUserInfo(data); //logs out response to the console
 };
+gitUserInfo();  //call this function to see results
 
-gitUserInfo();
-
-const displayUserInfo = function (data) {
+//Function to fetch and display my info from github
+const displayUserInfo = function (data) {       //fucntion accepts the JSON data as a parameter
     const div = document.createElement("div");  // creates a new div with a class of user-info
     div.classList.add("user-info");
+    //populate the new div with elements
     div.innerHTML = `    
         <figure>
             <img alt="user avatar" src=${data.avatar_url} />
@@ -31,55 +33,60 @@ const displayUserInfo = function (data) {
         </div>
         `;
         overview.append(div);  //append the div to the overview element
-        gitRepos();  //call the function below
+        gitRepos();  //call the async function below
 };
 
-const gitRepos = async function() {   //select my repos and sort by recently updated up to 100
+//select my repos and sort by the most recently updated 100 repos
+const gitRepos = async function() {   
     const myRepos = await fetch(  `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
-    const repoData = await myRepos.json();  //return json respons
+    const repoData = await myRepos.json();  //return json response
     repoInfo(repoData);
 };
 
+//function to display info about each repo
 const repoInfo = function (repos) {  //using repos as the parameter allows the function to accept the data from last API call
-    filterInput.classList.remove("hide");
-    for (const repo of repos) {   //loop an create a list item of each repo
+    filterInput.classList.remove("hide");  //displays the input element
+    for (const repo of repos) {   //loop and create a list item for each repo
         const repoItem = document.createElement("li");   //creates a new HTML element
         repoItem.classList.add("repo");   //adds a new class
-        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
-        repoList.append(repoItem);  //appends the list item to the global variable that selects the unordered repos list
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`; //add an h3 element to our html with the name of the repo
+        repoList.append(repoItem);  //appends the repoItem to the global variable that selects the unordered repoList
     }
 };
 
+//click event 
 repoList.addEventListener("click", function(e) {
-    if(e.target.matches("h3")) {   //check if the element that was clicked on matches the <h3> element /name of the repo
+    if(e.target.matches("h3")) {   //conditional statement to check if the element that was clicked on matches the <h3> element /name of the repo
         const repoName = e.target.innerText;  //targets the innerText where the event happens
       //console.log(repoName);
-      repoSpecs(repoName);  //calling repoSpecs and passing repoName as an argument
+      repoSpecs(repoName);  //calling repoSpecs from below and passing repoName as an argument
     }
 });
 
+//function to get specific repo info
 const repoSpecs = async function(repoName) {
     const eachRepo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);  //fetch request grabs repo specific info
-    const repoInfo = await eachRepo.json();
-    console.log(repoInfo);
-    const fetchLanguages = await fetch(repoInfo.languages_url);  //fetching data from language property of my repoInfo
-    const languageData = await fetchLanguages.json();
+    const repoInfo = await eachRepo.json();  //repoInfo will resolve and save the JSON response
+    console.log(repoInfo); //logging out repoInfo
+    const fetchLanguages = await fetch(repoInfo.languages_url);  //fetching data from language_url of my repoInfo
+    const languageData = await fetchLanguages.json(); 
     console.log(languageData);
-    const languages = [];  //create an empty array to hold the languages
+    const languages = [];  //an empty array to hold the languages
     for(const language in languageData){  //loop through the languageData object
         languages.push(language);  //adds the languages to the end of the array
     }
-    displayRepoInfo(repoInfo, languages);  //call the function do display repo info and pass it the repoInfo object and languages array
+    displayRepoInfo(repoInfo, languages);  //call the function to display repo info and pass it the repoInfo object and languages array
 };
 
+//function to display specific repo info//
 const displayRepoInfo = function(repoInfo, languages) {
-   viewReposButton.classList.remove("hide");
+   viewReposButton.classList.remove("hide");  
     repoData.innerHTML = "";  //empty the the html of repo-data
     repoData.classList.remove("hide");  //shows the class of repoData
     myRepoInfo.classList.add("hide");  //hides the element with the class of "repos"
-    const div = document.createElement("div");  //create a new div
+    const div = document.createElement("div");  // div to add selected repos name, description, branch and link to its github code
         div.innerHTML = `
-        <h3>Name: ${repoInfo.name}</h3>
+        <h3>Name: ${repoInfo.name}</h3>                   
         <p>Description: ${repoInfo.description}</p>
         <p>Default Branch: ${repoInfo.default_branch}</p>
         <p>Languages: ${languages.join(",")}</p>
@@ -88,24 +95,25 @@ const displayRepoInfo = function(repoInfo, languages) {
         repoData.append(div);  //appends div to repoData
 };
 
-viewReposButton.addEventListener("click", function () {
-    myRepoInfo.classList.remove("hide");
-    repoData.classList.add("hide");
-    viewReposButton.classList.add("hide");
+viewReposButton.addEventListener("click", function () {  //click event related to the Back to Repo button
+    myRepoInfo.classList.remove("hide");    //displays the .repos class where all repo info appears
+    repoData.classList.add("hide");         //hides the individual repo data
+    viewReposButton.classList.add("hide");  //hides the 'back to repo' button
 });
 
-filterInput.addEventListener("input", function(e) {  //Dynamic Search
-    const searchBoxText = e.target.value; //variable to capture the value of the search text 
-    const repos = document.querySelectorAll(".repo");
-    const searchToLower = searchBoxText.toLowerCase();
+//adds an Input Event to the Search Box creating Dynamic Search
+filterInput.addEventListener("input", function(e) { //attaches input to filterInput and passes the event(e) to the callback function
+    const searchBoxText = e.target.value; //variable captures the value of the search text 
+    const repos = document.querySelectorAll(".repo"); //selects all elements on the page with a class of repo
+    const searchToLower = searchBoxText.toLowerCase(); //assigned lowercase value of the search text
 
 
-for(const repo of repos) {    //new variable to reassign the lowercase search text
-    const lowerInnerText = repo.innerText.toLowerCase();
+for(const repo of repos) {    //loop to reassign the lowercase search text
+    const lowerInnerText = repo.innerText.toLowerCase(); 
     if(lowerInnerText.includes(searchBoxText)) {  //see if lowercase repo text includes the lowercase search text
-        repo.classList.remove("hide");  //if it does, show it
+        repo.classList.remove("hide");  //if it does, show the repos that match the text you typed into the input
     } else {
-        repo.classList.add("hide");   //if it doesn't, hide it
+        repo.classList.add("hide");   //if it doesn't, hide those repos
     }
 }
 });
